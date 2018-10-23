31b3f29df83904653fb8d8f29f27a0a22cf1d270
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2018-10-23 11:11:41 -0300

    commit intermedio por no poder levantar nodemon

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 // Requires..librerias para que funcione algo
 var express = require('express');
 var mongoose = require('mongoose');
-//inicializar variables
+var bodyParser = require('body-parser');
 
+//inicializar variables
 var app = express();
 
+
+//Body Parser ----MIDLEWARE
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+
 //Coneccion a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true }, (err, res) => {
     if (err) throw err;
     console.log('Base de datos:\x1b[32m%s\x1b[0m', 'online');
 });
 
 // Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'peticion realizada correctamnte'
-    });
-});
-
-
-
+//middlewear
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
 
 // escuchar peticiones
-// app.listen(3000, function() {
-// ..
-// });
+
 app.listen(3000, () => {
     console.log('Express server,Puerto 3000:\x1b[32m%s\x1b[0m', 'online');
 });
