f5e8d2467e33758480e28552180b66994291af2e
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2019-08-01 22:19:37 -0300

    EnDeploy046

--- app.js
+++ app.js
@@ -29,12 +29,12 @@ server.app.use(bodyParser.json());
 // CORS
 // app.use(express.static(path.resolve(__dirname, '../public')));
 server.app.use(cors({ origin: true, credentials: true }));
-server.app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    next();
-});
+// server.app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+//     next();
+// });
 
 
 
