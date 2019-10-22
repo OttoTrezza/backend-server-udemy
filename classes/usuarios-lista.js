6dc12faae57c5cffaa99b612f24fe339582ab53d
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2019-10-22 13:44:26 -0300

    FINALY?0.0.1

--- classes/usuarios-lista.js
+++ classes/usuarios-lista.js
@@ -3,46 +3,45 @@ class UsuariosLista {
             this.lista = [];
         }
         // Agregar un usuario
-    agregar(usuarioIO) {
-        this.lista.push(usuarioIO);
-        console.log(this.lista);
-        return usuarioIO;
-    }
-    actualizarNombre(id, nombre, sala) {
-            for (let usuarioIO of this.lista) {
-                if (usuarioIO.id === id) {
-                    usuarioIO.nombre = nombre;
-                    if (!sala)
-                        usuarioIO.sala = 'sin-sala';
-                    else
-                        usuarioIO.sala = sala;
-                    break;
-                }
-            }
-            console.log('===== Actualizando usuarioIO ====');
+    agregar(usuarioLis) {
+            this.lista.push(usuarioLis);
             console.log(this.lista);
         }
+        // actualizarNombre(id, nombre, sala) {
+        //         for (let usuarioIO of this.lista) {
+        //             if (usuarioIO.id === id) {
+        //                 usuarioIO.nombre = nombre;
+        //                 if (!sala)
+        //                     usuarioIO.sala = 'sin-sala';
+        //                 else
+        //                     usuarioIO.sala = sala;
+        //                 break;
+        //             }
+        //         }
+        //         console.log('===== Actualizando usuarioIO ====');
+        //         console.log(this.lista);
+        //     }
         // Obtener lista de usuarios
     getLista() {
             console.log('getLista');
-            return this.lista.filter(usuarioIO => usuarioIO.nombre !== 'sin-nombre');
-        }
-        // Obtener un usuario
-    getUsuario(id) {
-            return this.lista.find(usuarioIO => usuarioIO.id === id);
+            return this.lista.filter(usuarioLis => usuarioLis.nombre !== 'sin-nombre');
         }
         // Obtener usuario en una sala en particular
     getUsuariosEnSala(sala) {
-        return this.lista.filter(usuarioIO => usuarioIO.sala === sala);
+        return this.lista.filter(usuarioLis => usuarioLis.sala === sala);
     }
     getSalas() {
-            return this.lista.filter(usuarioIO => usuarioIO.sala)
-                .map(usuarioIO.sala);
+        return "juegos";
+        //   return this.lista.filter(usuarioIO => usuarioIO.sala)
+        //     .map(usuarioIO.sala);
+    }
+    getUsuario(nombre) {
+            return this.lista.find(usuarioLis => usuarioLis.nombre === nombre);
         }
         // Borrar Usuario
-    borrarUsuario(id) {
+    borrarUsuario(nombre) {
         const tempUsuario = this.getUsuario(id);
-        this.lista = this.lista.filter(usuarioIO => usuarioIO.id !== id);
+        this.lista = this.lista.filter(usuarioLis => usuarioLis.nombre !== nombre);
         return tempUsuario;
     }
 }
